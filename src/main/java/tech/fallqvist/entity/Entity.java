ad7edbe29abd8d2e2128d3b01ef9f1050c46088c
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-06-11 17:29:14 +0200

    Add Player sprite

--- src/main/java/tech/fallqvist/entity/Entity.java
+++ src/main/java/tech/fallqvist/entity/Entity.java
@@ -1,11 +1,17 @@
 package tech.fallqvist.entity;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public abstract class Entity {
 
     private int x, y;
     private int speed;
+    private BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    private String direction;
+
+    public abstract void update();
+    public abstract void draw(Graphics2D graphics2D);
 
     public int getX() {
         return x;
@@ -34,6 +40,84 @@ public abstract class Entity {
         return this;
     }
 
-    public abstract void update();
-    public abstract void draw(Graphics2D graphics2D);
+    public BufferedImage getUp1() {
+        return up1;
+    }
+
+    public Entity setUp1(BufferedImage up1) {
+        this.up1 = up1;
+        return this;
+    }
+
+    public BufferedImage getUp2() {
+        return up2;
+    }
+
+    public Entity setUp2(BufferedImage up2) {
+        this.up2 = up2;
+        return this;
+    }
+
+    public BufferedImage getDown1() {
+        return down1;
+    }
+
+    public Entity setDown1(BufferedImage down1) {
+        this.down1 = down1;
+        return this;
+    }
+
+    public BufferedImage getDown2() {
+        return down2;
+    }
+
+    public Entity setDown2(BufferedImage down2) {
+        this.down2 = down2;
+        return this;
+    }
+
+    public BufferedImage getLeft1() {
+        return left1;
+    }
+
+    public Entity setLeft1(BufferedImage left1) {
+        this.left1 = left1;
+        return this;
+    }
+
+    public BufferedImage getLeft2() {
+        return left2;
+    }
+
+    public Entity setLeft2(BufferedImage left2) {
+        this.left2 = left2;
+        return this;
+    }
+
+    public BufferedImage getRight1() {
+        return right1;
+    }
+
+    public Entity setRight1(BufferedImage right1) {
+        this.right1 = right1;
+        return this;
+    }
+
+    public BufferedImage getRight2() {
+        return right2;
+    }
+
+    public Entity setRight2(BufferedImage right2) {
+        this.right2 = right2;
+        return this;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public Entity setDirection(String direction) {
+        this.direction = direction;
+        return this;
+    }
 }
--- src/main/java/tech/fallqvist/entity/Player.java
+++ src/main/java/tech/fallqvist/entity/Player.java
@@ -3,7 +3,11 @@ package tech.fallqvist.entity;
 import tech.fallqvist.GamePanel;
 import tech.fallqvist.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
 
 public class Player extends Entity {
 
@@ -15,33 +19,67 @@ public class Player extends Entity {
         this.keyHandler = keyHandler;
 
         setDefaultValues();
+        getPlayerImage();
     }
 
     public void setDefaultValues() {
         setX(100);
         setY(100);
         setSpeed(4);
+        setDirection("down");
+    }
+
+    public void getPlayerImage() {
+
+        try {
+            setUp1(ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/images/player/boy_up_1.png"))));
+            setUp2(ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/images/player/boy_up_2.png"))));
+            setDown1(ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/images/player/boy_down_1.png"))));
+            setDown2(ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/images/player/boy_down_2.png"))));
+            setLeft1(ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/images/player/boy_left_1.png"))));
+            setLeft2(ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/images/player/boy_left_2.png"))));
+            setRight1(ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/images/player/boy_right_1.png"))));
+            setRight2(ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/images/player/boy_right_2.png"))));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void update() {
+
         if (keyHandler.upPressed) {
+            setDirection("up");
             setY(getY() - getSpeed());
         }
         else if (keyHandler.downPressed) {
+            setDirection("down");
             setY(getY() + getSpeed());
         }
         else if (keyHandler.leftPressed) {
+            setDirection("left");
             setX(getX() - getSpeed());
         }
         else if (keyHandler.rightPressed) {
+            setDirection("right");
             setX(getX() + getSpeed());
         }
     }
 
     @Override
     public void draw(Graphics2D graphics2D) {
-        graphics2D.setColor(Color.WHITE);
-        graphics2D.fillRect(getX(), getY(), gamePanel.getTileSize(), gamePanel.getTileSize());
+
+        BufferedImage image = null;
+
+        switch (getDirection()) {
+            case "up" -> image = getUp1();
+            case "down" -> image = getDown1();
+            case "left" -> image = getLeft1();
+            case "right" -> image = getRight1();
+        }
+
+        graphics2D.drawImage(image, getX(), getY(), gamePanel.getTileSize(), gamePanel.getTileSize(), null);
+
     }
 }
