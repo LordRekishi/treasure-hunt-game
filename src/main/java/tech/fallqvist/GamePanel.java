4259f372e6ad4992a3cbd30caddb281e973dc335
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-06-11 15:53:02 +0200

    Add FPS counter (sout)

--- src/main/java/tech/fallqvist/GamePanel.java
+++ src/main/java/tech/fallqvist/GamePanel.java
@@ -41,16 +41,18 @@ public class GamePanel extends JPanel implements Runnable {
 
     @Override
     public void run() {
-
         double drawInterval = 1000000000 / FPS;
-
         double delta = 0;
         long lastTime = System.nanoTime();
         long currentTime;
 
+        long timer = 0;
+        int drawCount = 0;
+
         while (gameThread != null) {
             currentTime = System.nanoTime();
             delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
             lastTime = currentTime;
 
             if (delta >= 1) {
@@ -61,6 +63,13 @@ public class GamePanel extends JPanel implements Runnable {
                 repaint(); // Calls the paintComponent() method
 
                 delta--;
+                drawCount++;
+            }
+
+            if (timer >= 1000000000) {
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
             }
 
         }
