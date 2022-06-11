2fa3b753da83469fd8a432e7c3f1bb41f9fbb1a0
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-06-11 19:51:55 +0200

    Add map loader

--- src/main/java/tech/fallqvist/GamePanel.java
+++ src/main/java/tech/fallqvist/GamePanel.java
@@ -2,34 +2,28 @@ package tech.fallqvist;
 
 import tech.fallqvist.entity.Entity;
 import tech.fallqvist.entity.Player;
+import tech.fallqvist.tile.TileManager;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel implements Runnable {
 
-    // SCREEN SETTINGS
     private final int originalTileSize = 16; // 16x16 tile
     private final int scale = 3;
 
     private final int tileSize = originalTileSize * scale; // 48x48 tile
-    private final int maxScreenColumn = 16; // Tiles vertically
-    private final int maxScreenRow = 12; // Tiles horizontally
-    private final int screenWidth = tileSize * maxScreenColumn; // 768 px
-    private final int screenHeight = tileSize * maxScreenRow; // 576 px
+    private final int maxScreenColumns = 16;
+    private final int maxScreenRows = 12;
+    private final int screenWidth = tileSize * maxScreenColumns; // 768 px
+    private final int screenHeight = tileSize * maxScreenRows; // 576 px
 
-    // FPS
-    private int FPS = 60;
+    private final int FPS = 60;
 
-    private KeyHandler keyHandler = new KeyHandler();
+    private final TileManager tileManager = new TileManager(this);
+    private final KeyHandler keyHandler = new KeyHandler();
     private Thread gameThread;
-    private Entity player = new Player(this, keyHandler);
-
-
-    // Set player default position
-    private int playerX = 100;
-    private int playerY = 100;
-    private int playerSpeed = 4;
+    private final Entity player = new Player(this, keyHandler);
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -90,6 +84,7 @@ public class GamePanel extends JPanel implements Runnable {
 
         Graphics2D graphics2D = (Graphics2D) graphics;
 
+        tileManager.draw(graphics2D);
         player.draw(graphics2D);
 
         graphics2D.dispose();
@@ -97,5 +92,21 @@ public class GamePanel extends JPanel implements Runnable {
 
     public int getTileSize() {
         return tileSize;
+    }
+
+    public int getMaxScreenColumns() {
+        return maxScreenColumns;
+    }
+
+    public int getMaxScreenRows() {
+        return maxScreenRows;
+    }
+
+    public int getScreenWidth() {
+        return screenWidth;
+    }
+
+    public int getScreenHeight() {
+        return screenHeight;
     }
 }
