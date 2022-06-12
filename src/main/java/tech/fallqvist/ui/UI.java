0995e828fe2602922c0f6f3bb44e3ddf18a88254
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-06-12 23:55:52 +0200

    Add Time mechanic

--- src/main/java/tech/fallqvist/ui/UI.java
+++ src/main/java/tech/fallqvist/ui/UI.java
@@ -5,6 +5,9 @@ import tech.fallqvist.object.OBJ_Key;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
 public class UI {
 
@@ -15,6 +18,8 @@ public class UI {
     private String message;
     private int messageCounter;
     private boolean gameFinished = false;
+    private double playTime;
+    private final DecimalFormat decimalFormat;
 
     public UI(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
@@ -22,6 +27,7 @@ public class UI {
         this.arial_80B = new Font("Arial", Font.BOLD, 80);
         OBJ_Key key = new OBJ_Key();
         this.keyImage = key.getImage();
+        this.decimalFormat = new DecimalFormat("#0.00", new DecimalFormatSymbols(Locale.ENGLISH));
     }
 
     public void draw(Graphics2D graphics2D) {
@@ -29,25 +35,13 @@ public class UI {
         if (gameFinished) {
             drawFoundTreasureMessage(graphics2D);
             drawCongratulationsMessage(graphics2D);
+            drawPlayTimeMessage(graphics2D);
             stopGame();
 
         } else {
-            graphics2D.setFont(arial_40);
-            graphics2D.setColor(Color.WHITE);
-            graphics2D.drawImage(keyImage, gamePanel.getTileSize() / 2, gamePanel.getTileSize() / 2, gamePanel.getTileSize(), gamePanel.getTileSize(), null);
-            graphics2D.drawString("x " + gamePanel.getPlayer().getNumberOfKeys(), 74, 65);
-
-            if (messageOn) {
-                graphics2D.setFont(graphics2D.getFont().deriveFont(30F));
-                graphics2D.drawString(message, gamePanel.getTileSize() / 2, gamePanel.getTileSize() * 5);
-
-                messageCounter++;
-
-                if (messageCounter > 120) {
-                    messageCounter = 0;
-                    messageOn = false;
-                }
-            }
+            updateKeyUiElement(graphics2D);
+            displayPlayTime(graphics2D);
+            checkToDisplayMessage(graphics2D);
         }
     }
 
@@ -62,7 +56,7 @@ public class UI {
         text = "You found the treasure!";
 
         textLength = (int) graphics2D.getFontMetrics().getStringBounds(text, graphics2D).getWidth();
-        x = gamePanel.getScreenWidth() / 2 - textLength /2;
+        x = gamePanel.getScreenWidth() / 2 - textLength / 2;
         y = gamePanel.getScreenHeight() / 2 - (gamePanel.getTileSize() * 3);
 
         graphics2D.drawString(text, x, y);
@@ -85,8 +79,51 @@ public class UI {
         graphics2D.drawString(text, x, y);
     }
 
+    private void drawPlayTimeMessage(Graphics2D graphics2D) {
+        String text;
+        int textLength;
+        int x, y;
+
+        graphics2D.setFont(arial_40);
+        graphics2D.setColor(Color.WHITE);
+
+        text = "Your Time is: " + decimalFormat.format(playTime) + "!";
+
+        textLength = (int) graphics2D.getFontMetrics().getStringBounds(text, graphics2D).getWidth();
+        x = gamePanel.getScreenWidth() / 2 - textLength / 2;
+        y = gamePanel.getScreenHeight() / 2 + (gamePanel.getTileSize() * 4);
+
+        graphics2D.drawString(text, x, y);
+    }
+
     private void stopGame() {
         gamePanel.setGameThread(null);
+    }
+
+    private void updateKeyUiElement(Graphics2D graphics2D) {
+        graphics2D.setFont(arial_40);
+        graphics2D.setColor(Color.WHITE);
+        graphics2D.drawImage(keyImage, gamePanel.getTileSize() / 2, gamePanel.getTileSize() / 2, gamePanel.getTileSize(), gamePanel.getTileSize(), null);
+        graphics2D.drawString("x " + gamePanel.getPlayer().getNumberOfKeys(), 74, 65);
+    }
+
+    private void displayPlayTime(Graphics2D graphics2D) {
+        playTime += (double) 1/60;
+        graphics2D.drawString("Time: " + decimalFormat.format(playTime), gamePanel.getTileSize() * 11, 65);
+    }
+
+    private void checkToDisplayMessage(Graphics2D graphics2D) {
+        if (messageOn) {
+            graphics2D.setFont(graphics2D.getFont().deriveFont(30F));
+            graphics2D.drawString(message, gamePanel.getTileSize() / 2, gamePanel.getTileSize() * 5);
+
+            messageCounter++;
+
+            if (messageCounter > 120) {
+                messageCounter = 0;
+                messageOn = false;
+            }
+        }
     }
 
     public void showMessage(String text) {
