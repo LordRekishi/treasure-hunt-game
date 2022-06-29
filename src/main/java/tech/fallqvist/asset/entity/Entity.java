33602fcb3e52178bc4c34636ebd84f6e17ec94e5
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-06-29 20:16:16 +0200

    Fix damage bug

--- src/main/java/tech/fallqvist/asset/entity/Entity.java
+++ src/main/java/tech/fallqvist/asset/entity/Entity.java
@@ -101,7 +101,7 @@ public abstract class Entity implements Asset {
 
         if (this instanceof MON_GreenSlime && contactPlayer) {
             if (!gamePanel.getPlayer().isInvincible()) {
-                gamePanel.getPlayer().setCurrentLife(getCurrentLife() - 1);
+                gamePanel.getPlayer().setCurrentLife(gamePanel.getPlayer().getCurrentLife() - 1);
                 gamePanel.getPlayer().setInvincible(true);
             }
         }
