758217ec5a6f5bde481419b19c5a8c7dc45b9a6c
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-07-24 13:53:43 +0200

    Add Weapon superclass

---
+++ src/main/java/tech/fallqvist/asset/object/equipment/Weapon.java
@@ -0,0 +1,32 @@
+package tech.fallqvist.asset.object.equipment;
+
+import tech.fallqvist.GamePanel;
+import tech.fallqvist.asset.object.Object;
+
+import java.awt.*;
+
+public class Weapon extends Object {
+
+    private Rectangle attackArea = new Rectangle(0, 0, 0, 0);
+    private int attackValue;
+
+    public Weapon(GamePanel gamePanel) {
+        super(gamePanel);
+    }
+
+    public Rectangle getAttackArea() {
+        return attackArea;
+    }
+
+    public void setAttackArea(Rectangle attackArea) {
+        this.attackArea = attackArea;
+    }
+
+    public int getAttackValue() {
+        return attackValue;
+    }
+
+    public void setAttackValue(int attackValue) {
+        this.attackValue = attackValue;
+    }
+}
