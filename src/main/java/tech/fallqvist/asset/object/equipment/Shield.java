ce7a9a33397248fa2d4e5835bce87b9c67a5b0a6
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-07-24 13:53:39 +0200

    Add Shield superclass

---
+++ src/main/java/tech/fallqvist/asset/object/equipment/Shield.java
@@ -0,0 +1,21 @@
+package tech.fallqvist.asset.object.equipment;
+
+import tech.fallqvist.GamePanel;
+import tech.fallqvist.asset.object.Object;
+
+public class Shield extends Object {
+
+    private int defenseValue;
+
+    public Shield(GamePanel gamePanel) {
+        super(gamePanel);
+    }
+
+    public int getDefenseValue() {
+        return defenseValue;
+    }
+
+    public void setDefenseValue(int defenseValue) {
+        this.defenseValue = defenseValue;
+    }
+}
